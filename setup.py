11065a374c0c27aaa4e335596b0a172f969765fd
unknown <w47593@PC78253.we.dirsrv.com>
2017-03-09 12:05:05 -0600

    Upload for example

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='wec_athena',
       version='0.1',
       description='Testing first package',
-      url='',
+      url='https://github.com/moone009/wec_athena',
       author='Christopher Mooney',
       author_email='mooneychristopher1@gmail.com',
       license='MIT',
