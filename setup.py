fb23fe53e79c19879ca56cb3c60e338352cb2661
unknown <w47593@PC78253.we.dirsrv.com>
2017-03-08 18:08:26 -0600

    Upload for example

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(name='wec_athena',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Testing first package',
       url='',
       author='Christopher Mooney',
-      author_email='flyingcircus@example.com',
+      author_email='mooneychristopher1@gmail.com',
       license='MIT',
       packages=['wec_athena'],
+      package_dir={'': 'C:\\users\\w47593'},
       zip_safe=False)
